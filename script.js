c0d139d013db7bce5865e42bcf4340a86dc37939
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-25 20:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+document.getElementById("button").onclick=function insert_Row() {
+	const tr = document.CreateElement("tr");
+	tr.innerHTML=`<td>${new cell}</td>`;
+	sampleTable.appendchild(tr); 
 }
+inert_Row();
