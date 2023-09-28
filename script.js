c7158f763106f34e693a5be15d3e779b5e610091
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-28 20:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-document.getElementById("button").onclick=function insert_Row() {
-	const tr = document.CreateElement("tr");
-	tr.innerHTML=`<td>${new cell}</td>`;
-	sampleTable.appendchild(tr); 
+function insert_Row() {
+    // Create a new row element
+	const curTableRow = document.getElementsByTagName("TR")[0];
+    const newRow = document.createElement("tr");
+
+    // Set the innerHTML of the new row with two cells
+    newRow.innerHTML = `<td>New Cell1</td><td>New Cell2</td>`;
+
+    // Get a reference to the table by its ID
+    //const table1 = document.getElementById("sampleTable");
+
+    // Insert the new row before the current row
+    document.getElementsByTagName("TABLE")[0].children[0].insertBefore(newRow, curTableRow);
+
+    // Update the current row to be the newly inserted ro
 }
-inert_Row();
